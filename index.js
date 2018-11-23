1b516e871592462583592397d487e6e9f8a8fd42
Star Bist <malimirkeccita@gmail.com>
2018-11-23 17:06:10 +0100

    Saving config to a local file.

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const log = require('./lib/log');
-const files = require('./lib/files');
-const inquirer = require('./lib/inquirer');
-const config = require('./lib/config');
+const log = require('./lib/log')
+const files = require('./lib/files')
+const inquirer = require('./lib/inquirer')
+const config = require('./lib/config')
 
-log.clear();
-log.figlet('IMPROVER', false);
-log.figlet('IMPROVER');
-
-log.message(config.getPath(), false);
+log.clear()
+log.figlet('IMPROVER', false)
 
 const run = async () => {
   const answers = await inquirer.startInteraction();
-  console.log(answers);
+  for (const key in answers) {
+    if (answers.hasOwnProperty(key)) {
+      config.setOptionValue(key, answers[key]);
+    }
+  }
+
+  log.message(config.save() || "Config saved successfully!");
 }
 
 run();
