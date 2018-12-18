d9ee3c6ef85a92e7c7258ceb3c20a6d991fd375f
Star Bist <malimirkeccita@gmail.com>
2018-12-18 16:13:12 +0100

    Wrapping all up. Basic CLI is finished.

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+#!/usr/bin/env node
+
+const program = require('commander')
 const log = require('./lib/log')
-const files = require('./lib/files')
 const inquirer = require('./lib/inquirer')
-const config = require('./lib/config')
+const sp = require('./lib/starter-project')
 
 log.clear()
-log.figlet('IMPROVER', false)
+log.figlet('S-PRO', false)
+log.message(`\n ** Starter Project CLI **\n`, false)
 
-const run = async () => {
-  const answers = await inquirer.startInteraction();
-  for (const key in answers) {
-    if (answers.hasOwnProperty(key)) {
-      config.setOptionValue(key, answers[key]);
-    }
-  }
+program
+  .version('0.0.1')
+  .description('Starter Project CLI')
 
-  log.message(config.save() || "Config saved successfully!");
-}
+program
+  .command('start')
+  .alias('s')
+  .description('Initialize Basic Starter Project')
+  .action(() => {
+    inquirer.basicInteraction().then((answers) => {
+      sp.run(answers)
+    })
+  })
 
-run();
+  // .command('starter-project-config')
+  // .alias('spro-conf')
+  // .description('Initialize Advanced Starter Project')
+  // .action(() => {
+  //   inquirer.advancedInteraction().then((answers) => {
+  //     sp.run(answers)
+  //   })
+  // })
+
+program.parse(process.argv)
