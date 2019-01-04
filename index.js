00f41af5a0c2db53c6e2c323b445446878d40366
Star Bist <malimirkeccita@gmail.com>
2019-01-04 17:53:11 +0100

    Package 1.0.9 - Fixing root path.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('1.0.8')
+  .version('1.0.9')
   .description('Starter Project CLI')
 
 program
