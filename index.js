8f74b1bbce3c7b1c81264deba2c976fbb99a946e
Star Bist <malimirkeccita@gmail.com>
2019-01-04 14:44:42 +0100

    Updating questions and default config.gs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('0.0.1')
+  .version('1.0.2')
   .description('Starter Project CLI')
 
 program
