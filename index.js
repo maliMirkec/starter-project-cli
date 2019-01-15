da5f5228f9efac7830c534f3684568c87209cf39
Star Bist <malimirkeccita@gmail.com>
2019-01-15 20:09:26 +0100

    Updating version.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('1.0.22')
+  .version('1.0.23-rc3')
   .description('Starter Project CLI')
 
 program
