4e6a6f8b3102c57e824c613894a453342b93d78c
Star Bist <malimirkeccita@gmail.com>
2019-01-27 15:59:39 +0100

    Package 1.0.24-rc8

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('1.0.24-rc7')
+  .version('1.0.24-rc8')
   .description('Starter Project CLI')
 
 program
