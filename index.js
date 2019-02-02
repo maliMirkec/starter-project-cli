6ec26bac4e938d1d1f8202e68e467f630e39e425
Star Bist <malimirkeccita@gmail.com>
2019-02-02 17:21:27 +0100

    Package 1.0.27

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('1.0.26')
+  .version('1.0.27')
   .description('Starter Project CLI')
 
 program
