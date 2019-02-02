53dd622b5cd166412dd880aeab40341918a8cad6
Star Bist <malimirkeccita@gmail.com>
2019-02-02 17:32:47 +0100

    Updating task condition.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('1.0.27')
+  .version('1.0.28')
   .description('Starter Project CLI')
 
 program
