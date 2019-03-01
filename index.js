a57dadc1e84f648ded819e06dd18a402303ee77d
Star Bist <malimirkeccita@gmail.com>
2019-03-01 13:17:59 +0100

    Package 1.0.32-rc1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ log.figlet('S-PRO', false)
 log.message(`\n ** Starter Project CLI **\n`, false)
 
 program
-  .version('1.0.31')
+  .version('1.0.32-rc1')
   .description('Starter Project CLI')
 
 program
