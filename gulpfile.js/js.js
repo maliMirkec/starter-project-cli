f040d8590934428f1a139c0c517f3cad6a11935b
Star Bist <malimirkeccita@gmail.com>
2019-05-20 08:47:30 +0200

    Moving to webpack.

--- gulpfile.js/js.js
+++ gulpfile.js/js.js
@@ -1,16 +1,17 @@
 const { src, dest, watch } = require('gulp')
 const gulpif = require('gulp-if')
 const babel = require('gulp-babel')
-const include = require('gulp-include')
 const eslint = global.config.js.lint ? require('gulp-eslint') : () => true
 const standard = global.config.js.lint ? require('gulp-standard') : () => true
 const sourcemaps = global.config.js.sourcemaps ? require('gulp-sourcemaps') : () => true
-const uglify = global.config.js.uglify ? require('gulp-uglify') : () => true
 const rename = global.config.js.uglify ? require('gulp-rename') : () => true
+const webpack = require('webpack')
+const gulpWebpack = require('webpack-stream')
 
 const { helpers } = require('./helpers')
 
 const jsConfig = require('./.js.json')
+const webpackConfig = require('./webpack.js')
 
 // gulp-if fix
 if (!global.config.css.sourcemaps) {
@@ -26,18 +27,15 @@ function jsStart () {
     })
     : {}
 
-  const thisIncludePaths = jsConfig.includeConfig.includePaths.map(path => helpers.parse(path))
-
-  const thisIncludeConfig = Object.assign({}, jsConfig.includeConfig, {
-    includePaths: thisIncludePaths
-  })
-
   if (!global.config.js.lint) {
     standard.reporter = () => true
     eslint.format = () => true
     eslint.failAfterError = () => true
     eslint.result = () => true
   }
+
+  webpackConfig.mode = (global.config.js.uglify) ? 'production' : 'development'
+  webpackConfig.devtool = (global.config.js.sourcemaps) ? 'sourcemaps' : ''
 
   return src(helpers.trim(`${helpers.source()}/${global.config.js.src}/*.js`))
     .pipe(gulpif(global.config.js.sourcemaps, sourcemaps.init()))
@@ -52,10 +50,12 @@ function jsStart () {
       console.warn(`[JS] Warnings: ${result.warningCount}`)
       console.error(`[JS] Errors: ${result.errorCount}`)
     })))
-    .pipe(include(thisIncludeConfig))
     .pipe(babel(jsConfig.babelConfig))
+    .pipe(
+      gulpWebpack(webpackConfig),
+      webpack
+    )
     .pipe(dest(helpers.trim(`${helpers.dist()}/${global.config.js.dist}`)))
-    .pipe(gulpif(global.config.js.uglify, uglify()))
     .pipe(gulpif(global.config.js.uglify, rename(jsConfig.renameConfig)))
     .pipe(gulpif(global.config.js.sourcemaps, sourcemaps.write(helpers.trim(`${helpers.source()}/${global.config.js.dist}`))))
     .pipe(dest(helpers.trim(`${helpers.dist()}/${global.config.js.dist}`)))
